394695a048d22b2b683bf67fdcf0f78773534c0a
orlvann <aorlova@edu.cdv.pl>
2023-03-22 16:30:40 +0100

    Added moving cube

---
+++ Assets/MoveCube.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rigidBody.AddForce(Vector3.up, ForceMode.Impulse);
+        }
+
+    }
+}
